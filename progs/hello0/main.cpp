d1dd31b62d643d29a27a0ac61e142946af6ac94f
Aurelien Larcher <aurelien.larcher@mines-paristech.fr>
2019-10-14 16:30:38 +0200

    Add arguments handling

--- progs/hello0/main.cpp
+++ progs/hello0/main.cpp
@@ -4,13 +4,29 @@
 /*
  * This program is a simple demonstration of C++
  */
-
-int main()
+int print_hello(int i)
 {
-	int i = 42;
 	// This is a legacy C example
 	printf("Hello World! %d\n", i);
 	// This is the now-preferred version
 	std::cout << "Hello World! " << i << "\n";
+	return 0;
+}
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Arg count : " << argc << "\n";
+	if (argc<2)
+	{
+		std::cout << "No argument\n";
+		return 1;
+	}
+	for (int a = 0; a < argc; ++a)
+	{
+		std::cout << a << ":" << argv[a] << "\n";
+	}
+	int i = atoi(argv[1]);
+	print_hello(i);
 	return 0;
 }
